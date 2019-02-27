29c65741b09441f7f2a33ec85f4da5a1fdf4b384
WBX <wang__bx@163.com>
2019-02-27 10:28:59 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var tabs = document.getElementById("tabs").getElementsByTagName("li");
+console.log(tabs);
+var lists = document.getElementById("lists").getElementsByTagName("ul");
+console.log(lists);
+for (var i = 0; i < tabs.length;i++){
+    tabs[i].onclick = showlist;
+}
+var scrollTop = document.documentElement.scrollTop;
+console.log(scrollTop);
+
+function showlist(){
+    for (var i = 0; i < tabs.length; i++ ) {
+        if(tabs[i] === this){
+            tabs[i].className = "active";
+            lists[i].className = "clearfix active";
+        }
+        else{
+            tabs[i].className ="";
+            lists[i].className = "clearfix";
+        }
+    }
+}
+ var seckillNav = document.getElementById("seckillNav");
+ window.onscroll = function(){
+     var scrollTop = document.documentElement.scrollTop||document.body.scrollTop||window.pageYOffset;
+     //ieee-》 document.body.scrollTop;
+     //苹果-> window.pageYOffset;
+     if( scrollTop >= 260){
+         seckillNav.className="seckill-nav seckill-navfixed";
+     }else{
+         seckillNav.className="seckill-nav";
+     }
+       console.log(scrollTop);
+}
